98f79d3b8199e71c6da687cf5d56d60045b4854a
Kamil Dawid <koxxor12@gmail.com>
2021-05-28 00:21:19 +0200

    First branch and excercise 13.1

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/Main.java
@@ -0,0 +1,22 @@
+package com.kodilla.good.patterns.challenges;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        MovieStory movieStory = new MovieStory();
+        Map<String, List<String>> booksTitlesWithTranslations = new HashMap<>();
+        booksTitlesWithTranslations = movieStory.getMovies();
+
+        booksTitlesWithTranslations.entrySet().stream()
+                .flatMap(entry -> entry.getValue().stream())
+                .forEach(s -> System.out.print(s + "!"));
+
+
+    }
+
+}
