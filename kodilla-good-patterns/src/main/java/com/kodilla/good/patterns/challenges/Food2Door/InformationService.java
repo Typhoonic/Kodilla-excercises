4b0fc20d183998b36087689744a4516c52984802
Kamil Dawid <koxxor12@gmail.com>
2021-06-01 17:40:15 +0200

    Excercise 13.4

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/Food2Door/InformationService.java
@@ -0,0 +1,2 @@
+package com.kodilla.good.patterns.challenges.Food2Door;public interface InformationService {
+}
---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/Food2Door/MailService.java
@@ -0,0 +1,10 @@
+package com.kodilla.good.patterns.challenges.Food2Door;
+
+public class MailService implements InformationService{
+    @Override
+    public void inform(User user) {
+
+        System.out.println("Dear " + user.getName() + "! Your order was end, we hope that all were going good.");
+
+    }
+}
