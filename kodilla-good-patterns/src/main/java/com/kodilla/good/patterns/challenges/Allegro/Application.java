c99c5f6b01cf4643e9b6db590107ad7d891ea089
Kamil Dawid <koxxor12@gmail.com>
2021-06-07 11:47:16 +0200

    Add another imports

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/Allegro/Application.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/Allegro/Application.java
@@ -1,12 +1,17 @@
-package com.kodilla.good.patterns.challenges;
+package com.kodilla.good.patterns.challenges.Allegro;
 
+import com.kodilla.good.patterns.challenges.MailService;
+import com.kodilla.good.patterns.challenges.OrderProcessor;
+import com.kodilla.good.patterns.challenges.OrderRequestRetriever;
+import com.kodilla.good.patterns.challenges.PaymentService;
+import com.kodilla.good.patterns.challenges.ProductOrderRepository;
 
 public class Application {
 
     public static void main(String[] args) {
 
         OrderRequestRetriever orderRequestRetriever = new OrderRequestRetriever();
-        OrderProcessor orderProcessor = new OrderProcessor(new MailService(), new ProductOrderRepository(), new OrderRequest(), new PaymentService());
+        OrderProcessor orderProcessor = new OrderProcessor(new MailService(), new ProductOrderRepository(), new com.kodilla.good.patterns.challenges.OrderRequest(), new PaymentService());
         orderProcessor.process(orderRequestRetriever.getUser(), orderRequestRetriever.getLocalDateTime(), orderRequestRetriever.getProduct());
 
 }}
