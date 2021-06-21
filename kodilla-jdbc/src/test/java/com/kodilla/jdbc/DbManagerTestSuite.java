56719f07893133bc93d0a4d5665b4fd6ace2f3d6
Kamil Dawid <koxxor12@gmail.com>
2021-06-21 13:33:36 +0200

    Add all?

---
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
@@ -0,0 +1,69 @@
+package com.kodilla.jdbc;
+
+import org.junit.jupiter.api.Test;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DbManagerTestSuite {
+
+    @Test
+    void testSelectUsersAndPosts() throws SQLException{
+
+        //Given
+        DbManager dbManager = DbManager.getInstance();
+        //When
+        String sqlQuery = "SELECT U.FIRSTNAME, U.LASTNAME, COUNT(*) AS POSTS\n" +
+                "FROM USERS U JOIN POSTS P on U.ID = P.USER_ID\n" +
+                "GROUP BY P.USER_ID\n" +
+                "HAVING COUNT(*) >= 2;";
+        Statement statement = dbManager.getConnection().createStatement();
+        ResultSet rs = statement.executeQuery(sqlQuery);
+        //Then
+        int counter = 0;
+        while(rs.next()){
+            System.out.println(rs.getString("FIRSTNAME") + ", " + rs.getString("LASTNAME") + ", " + rs.getInt("POSTS"));
+            counter ++;
+        }
+        statement.close();
+        rs.close();
+        assertEquals(2, counter);
+
+    }
+
+    @Test
+    void testSelectUser() throws SQLException{
+
+        //Given
+        DbManager dbManager = DbManager.getInstance();
+        //When
+        String sqlQuery = "SELECT * FROM USERS";
+        Statement statement = dbManager.getConnection().createStatement();
+        ResultSet rs = statement.executeQuery(sqlQuery);
+        //Then
+        int counter = 0;
+        while(rs.next()){
+            System.out.println(rs.getInt("ID") + ", " + rs.getString("FIRSTNAME") + ", " + rs.getString("LASTNAME"));
+            counter ++;
+        }
+        rs.close();
+        statement.close();
+        assertEquals(4, counter);
+
+    }
+
+
+    @Test
+    void testConnect() throws SQLException {
+
+        //Given
+        //When
+        DbManager dbManager = DbManager.getInstance();
+        //Then
+        assertNotNull(dbManager.getConnection());
+
+    }
+}
