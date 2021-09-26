9aa30cdb823f0c39591d2aa10b428a27c9cd3664
Typhoonic <81630895+Typhoonic@users.noreply.github.com>
2021-09-26 14:59:32 +0200

    [24.3] - AOP programming

--- kodilla-patterns2/src/main/java/com/kodilla/patterns2/facade/api/OrderFacade.java
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/facade/api/OrderFacade.java
@@ -1,6 +1,10 @@
 package com.kodilla.patterns2.facade.api;
 
 import com.kodilla.patterns2.facade.ShopService;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +13,7 @@ import org.springframework.stereotype.Service;
 import java.math.BigDecimal;
 
 @Service
+@Aspect
 public class OrderFacade {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(OrderFacade.class);
@@ -59,5 +64,45 @@ public class OrderFacade {
                 shopService.cancelOrder(orderId);
             }
         }
+    }
+
+    @Before("execution(* com.kodilla.patterns2.facade.ShopService.openOrder(..))")
+    public void eventOpenOrder(){
+        LOGGER.info("Trying to open order");
+    }
+
+    @AfterReturning("execution(* com.kodilla.patterns2.facade.ShopService.addItem(..))")
+    public void eventAddItem(){
+        LOGGER.info("Item was added");
+    }
+
+    @AfterReturning("execution(* com.kodilla.patterns2.facade.ShopService.removeItem(..))")
+    public void eventRemoveItem(){
+        LOGGER.info("Item was removed");
+    }
+
+    @Before("execution(* com.kodilla.patterns2.facade.ShopService.calculateValue(..))")
+    public void eventCalculateValue(){
+        LOGGER.info("Trying to calculate value");
+    }
+
+    @Before("execution(* com.kodilla.patterns2.facade.ShopService.doPayment(..))")
+    public void eventDoPayment(){
+        LOGGER.info("Need to make payment");
+    }
+
+    @AfterReturning("execution(* com.kodilla.patterns2.facade.ShopService.verifyOrder(..))")
+    public void eventVerifyOrder(){
+        LOGGER.info("Order was verified");
+    }
+
+    @AfterReturning("execution(* com.kodilla.patterns2.facade.ShopService.submitOrder(..))")
+    public void eventSubmitOrder(){
+        LOGGER.info("Try to submit order");
+    }
+
+    @AfterReturning("execution(* com.kodilla.patterns2.facade.ShopService.cancelOrder(..))")
+    public void eventCancelOrder(){
+        LOGGER.info("Order was cancelled");
     }
 }
