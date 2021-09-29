f61c073f92d02cf9f3dc55d97beb79af96b48bb0
Typhoonic <81630895+Typhoonic@users.noreply.github.com>
2021-09-29 23:56:50 +0200

    Fix ForumTopic

--- kodilla-patterns2/src/main/java/com/kodilla/patterns2/observer/forum/ForumTopic.java
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/observer/forum/ForumTopic.java
@@ -3,7 +3,7 @@ package com.kodilla.patterns2.observer.forum;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ForumTopic implements Observable{
+public class ForumTopic implements Observable {
 
     private final List<Observer> observers;
     private final List<String> messages;
@@ -35,7 +35,7 @@ public class ForumTopic implements Observable{
 
     @Override
     public void notifyObservers() {
-        for(Observer observer: observers)
+        for (Observer observer: observers)
             observer.update(this);
     }
 
