2bbb5150d0291c6af74008a5c4feceea241ed698
Typhoonic <81630895+Typhoonic@users.noreply.github.com>
2021-10-09 19:00:36 +0200

    [30.3] - testUpdateBestsellers

--- kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredProcTestSuite.java
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredProcTestSuite.java
@@ -11,6 +11,28 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class StoredProcTestSuite {
 
     @Test
+    public void testUpdateBestsellers() throws SQLException {
+        //Given
+        DbManager dbManager = DbManager.getInstance();
+        Statement statement = dbManager.getConnection().createStatement();
+        String sqlUpdate = "UPDATE exercises.BOOKS SET BESTSELLER = 0";
+        statement.executeUpdate(sqlUpdate);
+        String sqlCheckTable = "SELECT COUNT(*) AS HOW_MANY FROM exercises.BOOKS WHERE BESTSELLER = 0";
+        //When
+        String sqlProcedureCall = "CALL exercises.UpdateBestsellers()";
+        statement.execute(sqlProcedureCall);
+        ResultSet rs = statement.executeQuery(sqlCheckTable);
+        //Then
+        int howMany = -1;
+        if(rs.next()){
+            howMany = rs.getInt("HOW_MANY");
+        }
+        assertEquals(3, howMany);
+        //rs.close();
+        statement.close();
+    }
+
+    @Test
     public void testUpdateVipLevels() throws SQLException {
         // Given
         DbManager dbManager = DbManager.getInstance();
@@ -18,12 +40,10 @@ public class StoredProcTestSuite {
         Statement statement = dbManager.getConnection().createStatement();
         statement.executeUpdate(sqlUpdate);
         String sqlCheckTable = "SELECT COUNT(*) AS HOW_MANY FROM exercises.READERS WHERE VIP_LEVEL=\"Not set\"";
-        ResultSet rs = statement.executeQuery(sqlCheckTable);
-
         // When
         String sqlProcedureCall = "CALL exercises.UpdateVipLevels()";
         statement.execute(sqlProcedureCall);
-
+        ResultSet rs = statement.executeQuery(sqlCheckTable);
         // Then
         int howMany = -1;
         if (rs.next()) {
